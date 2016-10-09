b830e2476cce19ba27f7e1eef3e8cc26da48431d
sp0gg <joe.nelson@runbox.com>
2016-10-09 17:03:03 -0400

    getMin and getMax implemented

--- src/algorithms_data_structures_i/binary_search_tree/App.java
+++ src/algorithms_data_structures_i/binary_search_tree/App.java
@@ -7,15 +7,18 @@ public class App {
     public static void main(String[] args) {
         Tree<Integer> bst = new BinarySearchTree<>();
 
-        bst.insert(2);
+        bst.insert(7);
         bst.insert(1);
         bst.insert(3);
-        bst.insert(7);
+        bst.insert(25);
         bst.insert(5);
         bst.insert(-1);
         bst.insert(10);
         bst.insert(15);
+        bst.insert(80);
 
+        System.out.println(bst.getMax());
+        System.out.println(bst.getMin());
         bst.traverse();
     }
 
--- src/algorithms_data_structures_i/binary_search_tree/BinarySearchTree.java
+++ src/algorithms_data_structures_i/binary_search_tree/BinarySearchTree.java
@@ -3,28 +3,12 @@ package algorithms_data_structures_i.binary_search_tree;
 /**
  * Created by sp0gg on 10/9/16.
  */
-public class BinarySearchTree<T extends Comparable<T>> implements Tree<T> {
+class BinarySearchTree<T extends Comparable<T>> implements Tree<T> {
 
     private Node<T> root;
 
     public void traverse() {
 
-    }
-
-    public void insertNode(T insertionData, Node<T> node){
-        if (insertionData.compareTo(node.getData()) < 0) {
-            if (node.hasLeftChildNode()) {
-                insertNode(insertionData, node.getLeftChild());
-            }else{
-                node.setLeftChild(new Node<>(insertionData));
-            }
-        } else if (insertionData.compareTo(node.getData()) > 0) {
-            if (node.hasRightChildNode()) {
-                insertNode(insertionData, node.getRightChild());
-            }else{
-                node.setRightChild(new Node<>(insertionData));
-            }
-        }
     }
 
     public void insert(T data) {
@@ -40,10 +24,50 @@ public class BinarySearchTree<T extends Comparable<T>> implements Tree<T> {
     }
 
     public T getMax() {
-        return null;
+        if(!root.hasRightChildNode()){
+            return root.getData();
+        }else{
+            return getRightNode(root.getRightChild());
+        }
     }
 
     public T getMin() {
-        return null;
+        if(!root.hasLeftChildNode()){
+            return root.getData();
+        }else{
+            return getLeftNode(root.getLeftChild());
+        }
+    }
+
+    private T getRightNode(Node<T> node) {
+        if (node.hasRightChildNode()) {
+            return getRightNode(node.getRightChild());
+        }else{
+            return node.getData();
+        }
+    }
+
+    private T getLeftNode(Node<T> node) {
+        if (node.hasLeftChildNode()) {
+            return getLeftNode(node.getLeftChild());
+        }else{
+            return node.getData();
+        }
+    }
+
+    private void insertNode(T insertionData, Node<T> node){
+        if (insertionData.compareTo(node.getData()) < 0) {
+            if (node.hasLeftChildNode()) {
+                insertNode(insertionData, node.getLeftChild());
+            }else{
+                node.setLeftChild(new Node<>(insertionData));
+            }
+        } else if (insertionData.compareTo(node.getData()) > 0) {
+            if (node.hasRightChildNode()) {
+                insertNode(insertionData, node.getRightChild());
+            }else{
+                node.setRightChild(new Node<>(insertionData));
+            }
+        }
     }
 }
